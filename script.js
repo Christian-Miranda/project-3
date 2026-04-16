9d5bf0e9980bd2dcde93f39188383a0c7bcb7efa
Christian-Miranda <zw68q59v72@privaterelay.appleid.com>
2026-04-15 19:28:53 -0500

    added location

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+    center: { lat: 41.833588023525955, lng: -87.62430311366923 },
     zoom: 8,
   });
 }
