8b49dd9761e88141311a23dcbf0c7c97e476abd5
Christian-Miranda <zw68q59v72@privaterelay.appleid.com>
2026-04-15 22:57:24 -0500

    added new key, css, js

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
 let map;
+let houseLocation;
 
 async function initMap() {
-  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
+  houseLocation = { lat: 41.833008105893384, lng: -87.62415846783117 };
 
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 41.833588023525955, lng: -87.62430311366923 },
+    center: houseLocation,
     zoom: 15,
+    mapId: "81478dee2b879972e3b00e60"
   });
 
-  const position = { lat: 41.833588023525955, lng: -87.62430311366923 };
+  const { AdvancedMarkerElement, PinElement } =
+    await google.maps.importLibrary("marker");
 
   const pin = new PinElement({
     background: "#3F3C49",
-    borderColor: "#EE2AA9",
     glyphColor: "#F6FF35",
+    borderColor: "#EE2AA9",
   });
 
   const marker = new AdvancedMarkerElement({
     map: map,
-    position: position,
+    position: houseLocation,
     title: "Here is the house",
     content: pin.element,
   });
-  
+
+  const infoWindow = new google.maps.InfoWindow({
+    content: "This is the Phi Kappa Sigma house."
+  });
+
+  infoWindow.open({
+    map: map,
+    anchor: marker
+  });
+
+  var button = document.getElementById("recenter-btn");
+
+  if (button) {
+    button.onclick = function () {
+      map.setCenter(houseLocation);
+      map.setZoom(15);
+    };
+  }
 }
 
 window.initMap = initMap;
