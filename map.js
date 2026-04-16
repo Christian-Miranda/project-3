ff8babb7a96d07c1c551276114c1bc24794878d2
Christian-Miranda <zw68q59v72@privaterelay.appleid.com>
2026-04-15 19:11:44 -0500

    test js copy paste

--- map.js
+++ map.js
@@ -1,7 +1,10 @@
-var map;
+let map;
+
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat:41.83342813878343, lng: -87.62404560966976},
-    zoom: 8
-  };
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
 }
+
+window.initMap = initMap;
